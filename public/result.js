af641fa3ab49f389110924b9629761e6a452362f
Unidentified-TF <59943123+Unidentified-TF@users.noreply.github.com>
2020-01-25 02:46:32 +0000

    Update result.js

--- public/result.js
+++ public/result.js
@@ -10,10 +10,11 @@ var animalTF = ["skunk", "cow", "dog", "wolf", "cat", "tiger", "lion", "horse", 
 				"kangaroo", "elephant", "rat", "mouse", "hyena", "octopus", "panda", "bear", "hippo", "rhino", "snail", "spider", "bee", "crocodile", "bat", "zebra",
 				"crab", "dolphin", "deer", "monkey", "koala", "lizard", "moose", "moth", "orca", "otter", "parrot", "penguin", "poodle", "seal", "fish", "turtle",
 				"walrus", "beaver", "camel", "raccoon", "gazelle", "fox", "salamander", "bunny", "lemur", "scorpion", "llama", "squirrel", "panther", "sabretooth tiger",
-				"axolotl", "goose", "turkey", "chameleon", "anteater", "butterfly", "mantis", "slug", "cow", "horse", "porcupine"];
+				"axolotl", "goose", "turkey", "chameleon", "anteater", "butterfly", "mantis", "slug", "cow", "horse", "porcupine", "polar bear", "whale", "owl", "swan", 
+				"jackal", "husky", "tanuki", "red panda", "ferret", "gorilla", "pangolin", "ostrich", "armadillo", "peacock", "sloth"];
 
 var creatureTF = ["centaur", "dragon", "unicorn", "dinosaur", "succubus", "alien", "elf", "sphinx", "hellhound", "cerberus", "harpy", "hydra", "naga", "gryphon", "pegasus", 
-				  "gargoyle", "goblin", "orc", "chimera", "slime", "cyclops", "mermaid", "dryad", "minotaur", "genie", "fairy", "drider", "chocobo"];
+				  "gargoyle", "goblin", "orc", "chimera", "slime", "cyclops", "mermaid", "dryad", "minotaur", "genie", "fairy", "drider", "chocobo", "goddess", "satyr", "deathclaw"];
 
 var expansionTF = ["She transforms into a bimbo", "She gets pregnant", "Her breasts expand", "Her butt expands", "Her belly expands", "Her pussy expands", 
 				   "Her hair grows", "She grows into a giantess", "She shrinks down", "She unevenly grows bigger", "Her lips expand", "Her feet expand", "Her nipples expand",
@@ -27,13 +28,13 @@ var weirdTF = ["She grows extra breasts", "She grows extra arms", "She grows ext
 			   "She gets conjoined with another person", "She fuses with another person", "She transforms into a suckplant", "She transforms into a boob", 
 			   "Her head transforms into a dick, and her breasts fuse together forming a ballsack", "She turns into a dick and fuses with the nearest person", "She grows breasts from her back",
 			   "She splits into two shortstacks", "She grows a cock-tail", "She grows a big dick but instead of balls there are boobs", "Her hair turns into dicks", "Her hair turns into tentacles",
-			   "She grows multiple dicks", "Her belly button changes into a pussy", "Her nipples turn into taps and her boobs fill up with liquids", "Her nipples turn into tentacles"];
+			   "She grows multiple dicks", "Her belly button changes into a pussy", "Her nipples turn into taps and her boobs fill up with liquids", "Her nipples turn into tentacles", "She grows an udder"];
 
-var inanimateTF = ["pool-toy", "pumpkin", "toy-soldier", "panties", "sexdoll", "tree", "mousepad", "plushie", "flower", "onahole", "snowman", "doll", "statue", "socks",
+var inanimateTF = ["pool-toy", "pumpkin", "toy-soldier", "panties", "sexdoll", "tree", "mousepad", "plushie", "flower", "onahole", "snowman", "doll", "statue",
 				   "cake", "mushroom", "teddy bear", "pillow", "poster", "condom", "chair", "bra", "dildo", "cum", "car", "fountain", "mannequin", "candle", "pinata", 
-				   "costume", "chess piece"];
+				   "costume", "chess piece", "candy", "chocolate", "taxidermy head mount", "suit of themselves"];
 
-var otherTF = ["mime", "nerd", "robot", "drag queen", "MILF"];
+var otherTF = ["mime", "nerd", "robot", "drag queen", "MILF", "femboi"];
 
 //Stores all transformations the user has chosen
 var possibleTFs = [];
@@ -45,42 +46,41 @@ for(var i=0; i < inanimateTF.length; i++) inanimateTF[i] = "She transforms into 
 for(var i=0; i < otherTF.length; i++) otherTF[i] = "She transforms into a " + otherTF[i];
 
 //How the character reacts
-var reaction = ["aroused by", "confused about", "scared of", "shocked by", "terrified of", "embarrassed of", "surprised of", "happy with", "annoyed by", 
-				"excited about", "pissed about", "disappointed in", "nervous about", "amused by", "curious about", "interested in", "oblivious of", "angry with"];
+var reaction = ["aroused by", "confused about", "scared of", "shocked by", "terrified of", "embarrassed of", "surprised by", "happy with", "annoyed by", 
+				"excited about", "pissed about", "disappointed in", "nervous about", "amused by", "curious about", "very interested in", "oblivious of", "angry with"];
 
 //How the transformation is triggered
-var trigger = ["a spell", "a miscast spell", "pissing off a witch", "her family curse", "a strange morph app", "her allergy", "a virus outbreak", 
-			   "eating old food", "taking in new suppliments", "playing a bootleg video-game", "watching a movie", "listening to music", "nothing", "breaking something", 
-			   "an experiment", "drinking a new energy drink", "putting on cheap make-up", "playing a mobile game", "an infection she got from a scratch", "her new tattoo", "clicking on a pop-up advertisement", 
-			   "phrasing a wish badly", "exercising", "drinking a spiked drink", "being a asshole", "drinking a old potion", "not coming to work", 
-			   "having sexual intercourse", "sneezing", "playing a board game", "eating too much", "drinking homebrewed beer from a stranger", "being brainwashed", "trying out a new shampoo brand",
-			   "eating genetically modified food", "loosing a bet", "swimming in a lake", "the full moon", "breaking rules", "failing school", "breaking the law", 
-			   "being in the wrong place at the wrong time", "stepping in toxic waste", "coming across a mischievous genie", "lying to people", "being zapped by aliens", "a mad doctor", 
-			   "her being curious", "stepping into toxic waste", "looting cursed treasure", "using a VR headset", "being put into a mold", "a new spa treatment", 
-			   "a doctor who accidentally mixed up the clients", "reading a book", "wearing a costume from a pop-up store", "being caught spying", "a experiment gone wrong",
-			   "being brought back to life after dying", "loosing in a game show", "breaking a artifact", "using a off-brand controller", "putting on a mask", "loosing a game of poker", "a super villain", 
-			   "a teleporting accident", "a voodoo doll", "playing around with a 8-ball", "being hungry", "somehow being transported to another reality", "taking a selfie with a filter", "hitting her 'second' puberty",
-			   "being exposed to radiation", "being drunk", "volunteering", "trying on form fitting clothing", "opening a gift", "having a orgasm", "them playing with themself", 
-			   "getting a new job and needing to fit their standards", "opening spam email", "voulnteering at a magic show", "bit by someone suffering from the same transformation",
-			   "reading some old book", "angering a old woman", "drinking a limited edition latte", "winning the lottery", "vandalizing", "drinking too much", "doing drugs",
-			   "testing drugs for doctors", "being offered money for it", "misunderstanding things", "waking up after being cryogenically frozen for a 1000 years, and in need of a new body",
-			   "walking through a mirror", "looking at a funhouse mirror", "joining the circus", "pissing of a demon", "not following Unidentified-TF", "being sucked into a video-game",
-			   "failing a test", "a backfired prank", "her body adapting to survive", "dreaming it", "being possessed by a spirit", "her being curious", "wishing for a new life",
-			   "feeling like she was useless", "being annoying", "stealing", "a character creator in a video-game", "cheating on her significant other", "dating a magic user", 
-			   "sucked into a black hole", "getting piercings", "using new sex-toys", "saleswoman showcasing her products", "going into the sewers", "buying the wrong pills",
-			   "trying to prove magic isn't real", "kissing someone", "not showering for a long time", "a magic remote", "cosplaying", "breaking a magic wand", "using a knock-off sexdoll",
-			   "messing up a ritual", "magic clothing that won't come off", "looking at TF art", "watching porn", "being hypnotized", "wishing life was a bit more interesting", 
-			   "getting into a accident and having to get a new body", "eating fruits at a local fruit market", "being splashed with water", 
-			   "being accused of being a witch", "using sunscreen from a unknown company", "fantasizing", "blackmailing someone", "someone sneezing at them", "a role-player who slid into her DMs",
-			   "getting pranked by her geneticist friend", "having an affair with a magic user's significant other", "making a wish at a fortune telling machine",
-			   "blaspheming in earshot of a wrathful god", "being drafted into a new kind of branch", "selling her body", "buying a artifact"];
+var trigger = ["accidentally messing up a spell", "pissing off a witch", "their family curse kicking in,", "a strange morph app", "their allergy kicking in,", "contracting a virus", 
+			   "eating old food", "taking in new supplements", "playing a bootleg video-game", "watching a movie", "listening to music", "breaking a cursed object in a store/end", 
+			   "an experiment", "drinking a new energy drink", "putting on cheap make-up", "playing a mobile game", "an infection she got from a scratch", "getting a new tattoo", "clicking on a pop-up advertisement", 
+			   "phrasing a wish poorly", "exercising", "drinking a spiked drink", "being an asshole", "drinking a old potion", "not coming to work/end", "activating a trap in a tomb/end",
+			   "having sex", "sneezing", "playing a board game", "eating too much", "drinking homebrewed beer from a stranger", "being brainwashed", "trying out a new shampoo brand/end",
+			   "eating genetically modified food", "loosing a bet", "drinking dirty water", "the full moon", "breaking rules", "breaking the law and facing the consequences/end", "swimming in lake water/end",
+			   "being in the wrong place at the wrong time/end", "stepping in toxic waste", "coming across a mischievous genie", "lying to people", "being zapped by aliens", "a mad doctor looking for his next victim/end", 
+			   "stepping into toxic waste", "looting cursed treasure", "using a VR headset", "being pressed into a body shaping mold", "getting a free massage", "eating a strange plant on a space expedition/end",
+			   "a doctor who accidentally mixed up his clients/end", "reading a book", "wearing a costume she bought from a pop-up store", "being caught spying on a witch", "an experiment gone wrong",
+			   "being brought back to life after dying", "loosing in a game show/end", "breaking a artifact", "playing with a knock-off gameboy", "putting on a mask", "loosing a game of poker in a casino/end", "trying to stop a super-villain", 
+			   "being the first subject to test a teleporter/end", "someone modifying a voodoo doll of them", "playing around with a 8-ball", "being hungry", "being transported to another reality/end", "taking a selfie with a filter", "hitting a 'second' puberty",
+			   "being exposed to radiation", "being drunk", "volunteering", "putting on form-fitting clothing", "opening a gift", "having a orgasm", "masturbating", "walking into a cursed dungeon and getting locked until the changes are over/end",
+			   "getting a new job and needing to fit their standards/end", "opening spam email", "voulnteering at a magic show/end", "being bit by a weirdo", "being late to work/end",
+			   "reading some old book", "angering an old woman", "drinking a limited edition latte", "winning the lottery", "vandalizing", "drinking too much", "doing drugs", "testing drugs for money",
+			   "walking through a mirror", "looking at a funhouse mirror", "joining the circus/end", "pissing of a demon", "not following Unidentified-TF/end", "being sucked into a video-game",
+			   "their teacher punishing them for bad grades/end", "a backfired prank", "their body adapting to the surroundings", "thinking of it", "being possessed by a spirit", "being curious about how it would feel like,", "wishing for a new life",
+			   "a mischievous god", "being annoying", "stealing", "creating a character in a video-game", "cheating on their significant other", "dating a witch/end", "being struck by a lightning/end",
+			   "being sucked into a black hole, and then spit back/end", "getting piercings", "using new sex-toys", "salesman who knocked on their door to showcase his products/end", "going into the sewers/end", "using experimental pills",
+			   "trying to prove magic isn't real", "kissing someone", "having a bad hygiene", "playing with a magic remote", "cosplaying", "breaking a magic wand", "using a knock-off sexdoll",
+			   "messing up a ritual", "putting on magic clothing", "looking at transformation art", "watching porn", "being hypnotized", "wishing life was a bit more interesting", "a contamination on a space station/end",
+			   "eating fruits", "being splashed with water", "being exposed to toxic gas", "voulnteering to be a 'test rat' for DNA experimenting/end", "wanting fit in with the 'cool' people", 
+			   "being accused of being a witch", "using sunscreen from an unknown company", "fantasizing about it", "blackmailing someone", "someone sneezing at them", "getting a random DM asking about roleplaying",
+			   "getting pranked by their geneticist friend", "making a wish at a fortune telling machine/end", "a wish, and apparently everyone thinks she's normal and has been this way since birth../end", 
+			   "sneaking into a lab and accidentally breaking vials that released toxic fumes/end"];
 
 //Where the transformation occurs 
-var TFlocation = ["at a farm", "at home", "in a museum", "in a laboratory", "in a forest", "in a circus", "on the subway", "in the park", "in a classroom", 
-				  "on the beach", "in a theme park", "in a public bathroom", "in public", "at a party", "on the toilet", 
-				  "at the gym", "in a grocery store", "at a convention", "at a pool", "at work", "in the kitchen", "in her car", "in a elevator",
-				  "in a casino", "at a fancy restaurant", "on her couch", "in her garden", "at a race track", "in her bedroom", "in a back alley", "at a fair",
-				  "on her bed"];
+var TFlocation = ["at a farm", "at home", "in a museum", "in a laboratory", "while walking in a forest", "in a circus", "on the subway", "in the park", "in class", 
+				  "on the beach", "in a theme park", "in a public bathroom", "in public", "at a party", "while on the toilet", "while camping", "at a family reunion",
+				  "at the gym", "in a grocery store", "at a convention", "at a pool", "at work", "in the kitchen", "in a elevator", "at a magic show", "in a factory",
+				  "in a casino", "while eating at a fancy restaurant", "while sunbathing", "in her bedroom", "in a back alley", "at a fair", "on a plane", "on a train", "in a car",
+				  "at the city square", "at a bar", "in a church", "in a coffee shop", "in the woods", "at the zoo", "at the doctor's office"];
 
 //var reversal = ["is permanent!", "lasts for a month, but if she has sex... it'll become permanent", "lasts for 24 hours"]
 
@@ -130,9 +130,13 @@ function CreateCharacter()
 //Creates the transformation
 function Transformation()
 {
-	var resultstring = RandomValue(possibleTFs) + ", she is " + RandomValue(reaction) + " her transformation.<br>The transformation was caused by " + RandomValue(trigger);
-	if (document.getElementById("INCLlocation").checked === true) resultstring += " " + RandomValue(TFlocation);
-	//resultstring += "<br>The transformation " + RandomValue(reversal);
+	var resultstring = RandomValue(possibleTFs) + ", she is " + RandomValue(reaction) + " her transformation.<br>The transformation was caused by ";
+	var temp_trigg = RandomValue(trigger);
+	resultstring += temp_trigg.split('/end')[0];
+
+	if (temp_trigg.substr(temp_trigg.length - 4) != "/end") {
+		if (document.getElementById("INCLlocation").checked === true) resultstring += " " + RandomValue(TFlocation);
+	}
 	return resultstring;
 }
 
