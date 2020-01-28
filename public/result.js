0e166a468bb5a8c6e62d7909eac0566b99ed81c4
Unidentified-TF <59943123+Unidentified-TF@users.noreply.github.com>
2020-01-28 18:08:18 +0000

    Update result.js

--- public/result.js
+++ public/result.js
@@ -13,15 +13,20 @@ var animalTF = ["skunk", "cow", "dog", "wolf", "cat", "tiger", "lion", "horse", 
 				"crab", "dolphin", "deer", "monkey", "koala", "lizard", "moose", "moth", "orca", "otter", "parrot", "penguin", "poodle", "seal", "fish", "turtle",
 				"walrus", "beaver", "camel", "raccoon", "gazelle", "fox", "salamander", "bunny", "lemur", "scorpion", "llama", "squirrel", "panther", "sabretooth tiger",
 				"axolotl", "goose", "turkey", "chameleon", "anteater", "butterfly", "mantis", "slug", "cow", "horse", "porcupine", "polar bear", "whale", "owl", "swan", 
-				"jackal", "husky", "tanuki", "red panda", "ferret", "gorilla", "pangolin", "ostrich", "armadillo", "peacock", "sloth", "toad", "squid"];
+				"jackal", "husky", "tanuki", "red panda", "ferret", "gorilla", "pangolin", "ostrich", "armadillo", "peacock", "sloth", "toad", "squid", "bull", "stallion"];
 
 var creatureTF = ["centaur", "dragon", "unicorn", "dinosaur", "succubus", "alien", "elf", "sphinx", "hellhound", "cerberus", "harpy", "hydra", "naga", "gryphon", "pegasus", 
 				  "gargoyle", "goblin", "orc", "chimera", "slime", "cyclops", "mermaid", "dryad", "minotaur", "genie", "fairy", "drider", "chocobo", "goddess", "satyr", "deathclaw",
 				  "imp", "demon", "dwarf", "ghost", "ogre", "pixie", "siren", "vampire", "yeti"];
 
-var expansionTF = ["She transforms into a /extra/bimbo", "She gets pregnant", "Her breasts expand", "Her butt expands", "Her belly expands", "Her pussy expands", 
+var expansionTF = ["She transforms into a /extra/bimbo", "She gets pregnant", "Her breasts expand", "Her butt expands", "Her belly expands", "Her genitals expand", 
 				   "Her hair grows", "She grows into a /extra/giantess", "She shrinks down", "She unevenly grows bigger", "Her lips expand", "Her feet expand", "Her nipples expand",
 				   "Her tongue expands", "Her hips expand", "She transforms into a /extra/clown", "Her clit expands"];
+
+//Everything is written with "she", "her" and etc and then later replaced with the proper pronoun!
+var TGfem = ["Her pussy transforms into a dick", "She grows a dick above her pussy", "She transforms into a man", "She transforms into a femboi", "She transforms into a drag queen", "She transforms into a man", "She transforms into a man"];
+var TGmale = ["Her dick transforms into a pussy", "She grows a pussy beneath her dick", "She grows boobs", "She transforms into a woman", "She transforms into a bimbo", "She transforms into a amazoness", "She transforms into a pregnant woman", "She transforms into a woman", "She transforms into a woman"];
+var TGoth = ["She grow boobs", "She grow a pussy", "She grow a dick", "She get pregnant", "She transforms into a man", "She transforms into a woman"];
 
 var weirdTF = ["She grows multiple breasts", "She grows extra arms", "She grows extra legs", "She grows multiple pussies", "She grows extra eyes", "Her nipples turn into dicks",
 			   "Her buttcheeks transform into boobs", "Her feet transform into hands", "Her hands transform into feet", "She grows breasts all over her body", "Her nipples turn into pussies",
@@ -34,10 +39,10 @@ var weirdTF = ["She grows multiple breasts", "She grows extra arms", "She grows 
 			   "She grows multiple dicks", "Her belly button changes into a pussy", "Her nipples turn into taps and her boobs fill up with liquids", "Her nipples turn into tentacles", "She grows an udder"];
 
 var inanimateTF = ["/mod pool-toy", "pumpkin", "toy-soldier", "panties", "sexdoll", "tree", "mousepad", "/mod plushie", "flower", "onahole", "snowman", "doll", "statue",
-				   "cake", "mushroom", "teddy bear", "pillow", "poster", "condom", "chair", "bra", "dildo", "cum", "car", "fountain", "mannequin", "candle", "pinata", 
+				   "cake", "mushroom", "teddy bear", "pillow", "poster", "condom", "chair", "bra", "dildo", "cum-filled condom", "car", "fountain", "mannequin", "candle", "pinata", 
 				   "/mod costume", "chess piece", "candy", "chocolate", "/mod taxidermy head mount", "suit of themselves"];
 
-var otherTF = ["mime", "nerd", "robot", "drag queen", "MILF", "femboi", "amazoness"];
+var otherTF = ["mime", "nerd", "robot", "MILF", "amazoness"];
 
 //Stores all transformations the user has chosen
 var possibleTFs = [];
@@ -55,38 +60,38 @@ var reaction = ["aroused by", "confused about", "scared of", "shocked by", "terr
 				"excited about", "pissed about", "disappointed in", "nervous about", "amused by", "curious about", "very interested in", "oblivious of", "angry with", "indifferent about"];
 
 //How the transformation is triggered
-var trigger = ["accidentally messing up a spell", "pissing off a witch", "their family curse kicking in,", "a strange morph app", "their allergy kicking in,", "contracting a virus", 
+var trigger = ["accidentally messing up a spell", "pissing off a witch", "their family curse kicking in,", "using a strange morph app", "their allergy kicking in,", "contracting a virus", 
 			   "eating old food", "taking in new supplements", "playing a bootleg video-game", "watching a movie", "listening to music", "breaking a cursed object in a store/end", 
 			   "an experiment", "drinking a new energy drink", "putting on cheap make-up", "playing a mobile game", "an infection they got from a scratch", "getting a new tattoo", "clicking on a pop-up advertisement", 
 			   "phrasing a wish poorly", "exercising", "drinking a spiked drink", "being an asshole", "drinking a old potion", "not coming to work/end", "activating a trap in a tomb/end",
 			   "having sex", "sneezing", "playing a board game", "eating too much", "drinking homebrewed beer from a stranger", "being brainwashed", "trying out a new shampoo brand/end",
 			   "eating genetically modified food", "loosing a bet", "drinking dirty water", "being exposed to the full moon", "breaking rules", "breaking the law and facing the consequences/end", "swimming in lake water/end",
-			   "being in the wrong place at the wrong time/end", "stepping in toxic waste", "coming across a mischievous genie", "lying to people", "being zapped by aliens", "a mad doctor looking for his next victim/end", 
+			   "being in the wrong place at the wrong time/end", "stepping in toxic waste", "coming across a mischievous genie", "lying to people", "being zapped by aliens", "being a mad-doctor's next plaything/end", 
 			   "stepping into toxic waste", "looting cursed treasure", "using a VR headset", "being pressed into a body shaping mold", "getting a free massage", "eating a strange plant on a space expedition/end",
 			   "a doctor who accidentally mixed up his clients/end", "reading a book", "wearing a costume they bought from a pop-up store", "being caught spying on a witch", "an experiment gone wrong",
 			   "waking up from a coma", "loosing in a game show/end", "breaking a artifact", "playing with a knock-off gameboy", "putting on a mask", "loosing a game of poker in a casino/end", "trying to stop a super-villain", 
 			   "testing a teleporter/end", "someone modifying a voodoo doll of them", "playing around with a 8-ball", "being hungry", "being transported to another reality/end", "taking a selfie with a filter", "hitting a 'second' puberty",
-			   "being exposed to radiation", "being drunk", "volunteering", "putting on form-fitting clothing", "opening a gift", "having a orgasm", "masturbating", "walking into a cursed dungeon and getting locked until the changes are over/end",
+			   "being exposed to radiation", "being drunk", "volunteering", "putting on form-fitting clothing", "opening a gift", "having a orgasm", "masturbating", "being locked in a dungeon/end",
 			   "getting a new job and needing to fit their standards/end", "opening spam email", "volunteering at a magic show/end", "being bit by a weirdo", "being late to work/end",
 			   "reading some old book", "angering an old woman", "drinking a limited edition latte", "winning the lottery/end", "vandalizing", "drinking too much", "doing drugs", "testing drugs for money",
 			   "walking through a mirror", "looking at a funhouse mirror", "joining the circus/end", "pissing of a demon", "not following Unidentified-TF/end", "being sucked into a video-game",
 			   "their teacher punishing them for bad grades/end", "a backfired prank", "their body adapting to the surroundings", "thinking of it", "being possessed by a spirit", "being curious about how it would feel like,", "wishing for a new life",
 			   "a mischievous god", "being annoying", "stealing", "creating a character in a video-game", "cheating on their significant other", "dating a witch/end", "being struck by a lightning/end",
-			   "being sucked into a black hole, and then spit back/end", "getting piercings", "using new sex-toys", "salesman showcasing his products/end", "going into the sewers/end", "using experimental pills",
+			   "being sucked into a black hole, and then spit back/end", "getting piercings", "using new sex-toys", "a salesman showcasing his products/end", "going into the sewers/end", "using experimental pills",
 			   "trying to prove magic isn't real", "kissing someone", "having a bad hygiene", "playing with a magic remote", "cosplaying", "breaking a magic wand", "using a knock-off sexdoll",
 			   "messing up a ritual", "putting on magic clothing", "looking at transformation art", "watching porn", "being hypnotized", "wishing life was a bit more interesting", "a contamination on a space station/end",
 			   "eating fruits", "being splashed with water", "being exposed to toxic gas", "volunteering to be a 'test rat' for DNA experimenting/end", "wanting fit in with the 'cool' people", 
 			   "being accused of being a witch", "using sunscreen from an unknown company", "fantasizing about it", "blackmailing someone", "someone sneezing at them", "getting a random DM asking about roleplaying",
 			   "getting pranked by their geneticist friend", "making a wish at a fortune telling machine/end", "a wish, and apparently everyone thinks this is normal and they've been this way since birth../end", 
-			   "sneaking into a lab and accidentally breaking vials that released toxic fumes/end", "getting a vaccination/end", "using a unfinished teleporter at a laboratory/end", "being confused which one was the transformation-pod and which one was the cryo-pod/end",
+			   "sneaking into a lab and accidentally breaking vials that released toxic fumes/end", "getting a vaccination/end", "using a unfinished teleporter at a laboratory/end", "being confused between a transformation-pod and a cryo-pod/end",
 			   "walking through a portal that mysteriously opened up", "injecting themselves with experimental nanobots that were only supposed to improve their health", "being kidnapped by a mad doctor and being used as their 'test rat'/end",
-			   "using essential oils"];
+			   "using essential oils", "using this generator"];
 
 //Where the transformation occurs 
 var TFlocation = ["at a farm", "at home", "in a museum", "in a laboratory", "while walking in a forest", "in a circus", "on the subway", "in the park", "in class", 
 				  "on the beach", "in a theme park", "in a public bathroom", "in public", "at a party", "while on the toilet", "while camping", "at a family reunion",
 				  "at the gym", "in a grocery store", "at a convention", "at a pool", "at work", "in the kitchen", "in a elevator", "at a magic show", "in a factory",
-				  "in a casino", "while eating at a fancy restaurant", "while sunbathing", "in her bedroom", "in a back alley", "at a fair", "on a plane", "on a train", "in a car",
+				  "in a casino", "while eating at a fancy restaurant", "while sunbathing", "in their bedroom", "in a back alley", "at a fair", "on a plane", "on a train", "in a car",
 				  "at the city square", "at a bar", "in a church", "in a coffee shop", "in the woods", "at the zoo", "at the doctor's office", "at an arcade"];
 
 //var reversal = ["is permanent!", "lasts for a month, but if she has sex... it'll become permanent", "lasts for 24 hours"]
@@ -103,6 +108,13 @@ function randomize()
 	if (document.getElementById("animal").checked) possibleTFs = possibleTFs.concat(animalTF);
 	if (document.getElementById("creature").checked) possibleTFs = possibleTFs.concat(creatureTF);
 	if (document.getElementById("expansion").checked) possibleTFs = possibleTFs.concat(expansionTF);
+	if (document.getElementById("tg").checked) 
+	{
+		if (document.getElementById("female").checked === true) possibleTFs = possibleTFs.concat(TGfem);
+		if (document.getElementById("male").checked === true) possibleTFs = possibleTFs.concat(TGmale);
+		if (document.getElementById("other").checked === true) possibleTFs = possibleTFs.concat(TGoth);
+
+	}
 	if (document.getElementById("weird").checked) possibleTFs = possibleTFs.concat(weirdTF);
 	if (document.getElementById("inanimate").checked) possibleTFs = possibleTFs.concat(inanimateTF);
 	if (document.getElementById("tfother").checked) possibleTFs = possibleTFs.concat(otherTF);
@@ -169,18 +181,18 @@ function FinalResults()
 	document.getElementById("TFresult").innerHTML = resultstring;
 	if (characterresultstring != null) 
 	{
-		document.getElementById("CHARresult").innerHTML = Gender(characterresultstring);
-		THEresults = "Character:\n" + Gender(characterresultstring) + "\n\nTransformation:\n" + Gender(resultstring);
+		document.getElementById("CHARresult").innerHTML = Gender(characterresultstring, true) + ".";
+		THEresults = "Character:\n" + Gender(characterresultstring, true) + ".\n\nTransformation:\n" + Gender(resultstring);
 	}
 	else
 		THEresults = "Transformation:\n" + Gender(resultstring);
 }
 
 //Changes the gender of the character, default is female
-function Gender(thestring) {
+function Gender(thestring, char) {
 
-	var M_pronouns = ["his", "His", "he", "He", "he's", "man"];
-	var O_pronouns = ["their", "Their", "they", "They", "they're", "person"];
+	var M_pronouns = [" his ", " His ", " he ", " He ", " he's ", " man, "];
+	var O_pronouns = [" their ", " Their ", " they ", " They ", " they're ", " person, "];
 	var pronouns = [];
 
 	if (document.getElementById("female").checked === true) return thestring;
@@ -191,12 +203,16 @@ function Gender(thestring) {
 		thestring = thestring.replace("transforms", "transform");
 	}
 
-	thestring = thestring.replace("her", pronouns[0]);
-	thestring = thestring.replace("Her", pronouns[1]);
-	thestring = thestring.replace("she", pronouns[2]);
-	thestring = thestring.replace("She", pronouns[3]);
-	thestring = thestring.replace("she's", pronouns[4]);
-	thestring = thestring.replace("woman", pronouns[5]);
+	thestring = thestring.replace(/(?:^|\W)her(?:$|\W)/g, pronouns[0]);
+	thestring = thestring.replace(/(?:^|\W)Her(?:$|\W)/g, pronouns[1]);
+	thestring = thestring.replace(/(?:^|\W)she(?:$|\W)/g, pronouns[2]);
+	thestring = thestring.replace(/(?:^|\W)She(?:$|\W)/g, pronouns[3]);
+	thestring = thestring.replace(/(?:^|\W)she's(?:$|\W)/g, pronouns[4]);
+
+	thestring = thestring.replace(/(?:^|\W)he s(?:$|\W)/g, pronouns[4]);
+	thestring = thestring.replace(/(?:^|\W)they s(?:$|\W)/g, pronouns[4]);
+
+	if (char === true) thestring = thestring.replace(/(?:^|\W)woman(?:$|\W)/g, pronouns[5]);
 
 	if (document.getElementById("other").checked === true)
 	{
